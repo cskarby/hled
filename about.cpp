01bebfc811d6e912e112f2c3a2c7df1a8c38b5f3
Christian Skarby <christian.skarby@interhost.no>
2010-10-31 15:29:13 +0000

    Extend about to have more information.

--- about.cpp
+++ about.cpp
@@ -8,8 +8,8 @@ About::About(const QString &title, const QString &fileName, QWidget *parent) :
     this->setWindowTitle(title);
     QFile file(fileName);
     if (file.open(QFile::ReadOnly | QFile::Text)) {
-        ui.textBrowser->setPlainText(file.readAll());
+        ui.textBrowser->setPlainText(QString::fromUtf8(file.readAll()));
     }
-    ui.label->setText(qApp->applicationName() + " " + qApp->applicationVersion() + " created by Interhost AS - www.interhost.no.");
+    ui.label->setText(qApp->applicationName() + " " + qApp->applicationVersion() + " created by Interhost AS - www.interhost.no - +47  21 42 01 12.");
     connect(ui.pushButton, SIGNAL(clicked()), this, SLOT(hide()));
 }
